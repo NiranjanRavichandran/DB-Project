5568fd6fc195df86b79c2dce6be90079928649c6
Niranjan Ravichandran <niranjanravichandran@live.com>
2015-12-04 17:07:47 -0500

    Initial Commit

---
+++ login.php
@@ -0,0 +1,73 @@
+<!DOCTYPE HTML>
+
+<html>
+	<head>
+		<title>Tekket - Login</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="stylesheet" href="assets/css/custom.css" />
+	</head>
+	<body>
+
+		<!-- Page Wrapper -->
+			<div id="page-wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<h1><a href="index.php">Tekket</a></h1>
+						<nav>
+							<a href="#menu">Menu</a>
+						</nav>
+					</header>
+
+				<!-- Menu -->
+					<nav id="menu">
+						<div class="inner">
+							<h2>Menu</h2>
+							<ul class="links">
+								<li><a href="index.php">Home</a></li>
+								<li><a href="login.php">Log In</a></li>
+								<li><a href="signup.php">Sign Up</a></li>
+							</ul>
+							<a href="#" class="close">Close</a>
+						</div>
+					</nav>
+
+				<!-- Wrapper -->
+					<section id="wrapper">
+
+						<!-- Content -->
+							<div class="login-outter-wrap">
+								<div class="inner login-wrapper">
+									<h2 class="major">Tekket Login</h2>
+									<form method="post" action="#">
+										<div class="field">
+											<label for="name">Username</label>
+											<input type="text" name="username" id="username" />
+										</div>
+										<div class="field">
+											<label for="name">Password</label>
+											<input type="text" name="passowrd" id="password" />
+										</div>
+										<ul class="actions">
+											<li><input type="submit" value="Login" /></li>
+										</ul>
+									</form>
+								</div>
+							</div>
+
+					</section>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.scrollex.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
